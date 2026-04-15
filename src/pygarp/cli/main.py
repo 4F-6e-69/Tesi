8a552d14b1b46d174410c107f5e02f5b833aef2f
Angelo Radatti <radangelo23@gmail.com>
2026-04-15 12:30:05 +0200

    test esagono file c1

--- src/pygarp/cli/main.py
+++ src/pygarp/cli/main.py
@@ -30,7 +30,7 @@ scarf_dict = {
     "outline": True,
     "outline_style": "gradient",
 
-    "fill": True,
+    "fill": False,
     "fill_style": "grid",
     "fill_dir": 0.0,
     "fill_spacing": 8.0,
@@ -124,7 +124,7 @@ def plot_positions_and_orientations(filename: str, arrow_length: float = 5.0):
 @app.command("calc")
 def main():
     result = execute_pocketing_job(shape_config, space_config, scarf_config, robot_config)
-    filename = str(Path(__file__).resolve().parent.parent.parent.parent / "tests/output/esagono.csv")
+    filename = str(Path(__file__).resolve().parent.parent.parent.parent / "tests/output/esagono_con.csv")
     pocket_writing(result, filename)
     plot_positions_and_orientations(filename)
 
